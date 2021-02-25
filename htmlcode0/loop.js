5e17627f51311e4f14d43a9df8e335e87e1e8ecb
Bayanabuhatab <79096538+Bayanabuhatab@users.noreply.github.com>
2021-02-25 10:29:36 +0200

    Update loop.js

--- htmlcode0/loop.js
+++ htmlcode0/loop.js
@@ -10,7 +10,7 @@ var questionNum = prompt ("Are you sure?")
     if (questionNum =="yes"){
         alert("Then we will contact you very soon");
         for(var i=0; i < ask; i++){
-            document.write("<img src=https://vid.alarabiya.net/images/2020/11/25/4a35f783-5f0b-4720-9b2b-f2af588ef15a/4a35f783-5f0b-4720-9b2b-f2af588ef15a.jpg''  />")
+            document.write('<img src=https://vid.alarabiya.net/images/2020/11/25/4a35f783-5f0b-4720-9b2b-f2af588ef15a/4a35f783-5f0b-4720-9b2b-f2af588ef15a.jpg'/>")
         }
     }else{
         alert("Sorry we can not help you");
