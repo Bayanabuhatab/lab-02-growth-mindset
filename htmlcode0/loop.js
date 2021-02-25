c94715cf6db7157a476e1253c7f653afb57e06d3
Bayanabuhatab <79096538+Bayanabuhatab@users.noreply.github.com>
2021-02-25 10:41:58 +0200

    Update loop.js

--- htmlcode0/loop.js
+++ htmlcode0/loop.js
@@ -10,7 +10,7 @@ var questionNum = prompt ("Are you sure?")
     if (questionNum =="yes"){
         alert("Then we will contact you very soon");
         for(var i=0; i < ask; i++){
-            document.write("<img src=https://rotana.net/assets/uploads/2018/09/%D8%B4%D8%AC%D8%B1%D8%A9-%D8%A7%D9%84%D8%B4%D9%8A%D8%B7%D8%A7%D9%86.jpg'' />")
+            document.write("<img src='https://rotana.net/assets/uploads/2018/09/%D8%B4%D8%AC%D8%B1%D8%A9-%D8%A7%D9%84%D8%B4%D9%8A%D8%B7%D8%A7%D9%86.jpg' />")
         }
     }else{
         alert("Sorry we can not help you");
