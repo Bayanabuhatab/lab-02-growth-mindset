5393df6478696795fc61bc6f32c944c71f6d395e
bayanabuhatab <bayan.m.a@hotmail.com>
2021-02-25 09:46:42 +0200

    new creation

---
+++ htmlcode0/loop.js
@@ -0,0 +1,17 @@
+var ask = prompt ("how many trees do you need? it should be <5");
+function display(){
+for (i = 0; i< 9999; i++) {
+    ask = prompt ("how many trees do you need <5");
+if (ask > 5) {
+        break; 
+    }}}
+display()
+var questionNum = prompt ("Are you sure?") 
+    if (question =="yes"){
+        alert("Then we will contact you very soon");
+        for(var i=0; i < ask; i++){
+            document.write("<img src=https://vid.alarabiya.net/images/2020/11/25/4a35f783-5f0b-4720-9b2b-f2af588ef15a/4a35f783-5f0b-4720-9b2b-f2af588ef15a.jpg''  />")
+        }
+    }else{
+        alert("Sorry we can not help you");
+    }
